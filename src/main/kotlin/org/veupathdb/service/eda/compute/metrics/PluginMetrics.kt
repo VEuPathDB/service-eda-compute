8f6abd946828c0dd0f458fec7f1468908209b1a0
Elizabeth <foxcapade@gmail.com>
2022-07-08 11:18:29 -0400

    updates

--- src/main/kotlin/org/veupathdb/service/eda/compute/metrics/PluginMetrics.kt
+++ src/main/kotlin/org/veupathdb/service/eda/compute/metrics/PluginMetrics.kt
@@ -3,8 +3,24 @@ package org.veupathdb.service.eda.compute.metrics
 import io.prometheus.client.Counter
 import io.prometheus.client.Histogram
 
+/**
+ * Prometheus Metrics for Plugins
+ *
+ * These metrics add to the existing metrics exposed by the async platform.
+ *
+ * @author Elizabeth Paige Harper - https://github.com/foxcapades
+ * @since 1.0.0
+ */
 object PluginMetrics {
 
+  /**
+   * Histogram of plugin execution time.
+   *
+   * Time is measured in seconds.
+   *
+   * **NOTE**: This is not the full job execution time, this is specifically the
+   * time spent by the plugin implementation code.
+   */
   val execTime: Histogram = Histogram.build()
     .buckets(
       0.1,
@@ -27,12 +43,24 @@ object PluginMetrics {
     .help("Plugin execution time in seconds.")
     .register()
 
+  /**
+   * Counter of successful plugin executions.
+   *
+   * This differs from the async platform's `job_successes` metric in that it is
+   * keyed on plugin name rather than queue name.
+   */
   val successes: Counter = Counter.build()
     .labelNames("plugin_name")
     .name("plugin_successes")
     .help("Number of successful executions of a plugin")
     .register()
 
+  /**
+   * Counter of failed plugin executions.
+   *
+   * This differs from the async platform's `job_failures` metric in that it is
+   * keyed on the plugin name rather than the queue name.
+   */
   val failures: Counter = Counter.build()
     .labelNames("plugin_name")
     .name("plugin_failures")
