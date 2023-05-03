1d7dcf5b6cca0690579174fe20f1370cc3797ac2
Ryan Doherty <tech@conical.org>
2023-05-03 01:43:12 -0400

    Remove requirement on native var in example plugin so we can test derived vars

--- src/main/java/org/veupathdb/service/eda/compute/plugins/example/ExamplePluginInputValidator.java
+++ src/main/java/org/veupathdb/service/eda/compute/plugins/example/ExamplePluginInputValidator.java
@@ -1,6 +1,8 @@
 package org.veupathdb.service.eda.compute.plugins.example;
 
 import jakarta.ws.rs.BadRequestException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.veupathdb.service.eda.common.model.EntityDef;
 import org.veupathdb.service.eda.common.model.ReferenceMetadata;
 import org.veupathdb.service.eda.common.model.VariableDef;
@@ -13,11 +15,15 @@ import java.util.function.Supplier;
 
 public class ExamplePluginInputValidator implements PluginConfigValidator<ExamplePluginRequest> {
 
+  private static final Logger LOG = LogManager.getLogger(ExamplePluginInputValidator.class);
+
   @Override
   public void validate(ExamplePluginRequest request, Supplier<ReferenceMetadata> referenceMetadata) {
 
     ExampleComputeConfig config = request.getConfig();
     VariableSpec inputVar = config.getInputVariable();
+
+    LOG.info("Checking var " + VariableDef.toDotNotation(inputVar));
 
     // check entity
     EntityDef entity = referenceMetadata.get().getEntity(inputVar.getEntityId())
@@ -26,11 +32,6 @@ public class ExamplePluginInputValidator implements PluginConfigValidator<Exampl
     // check variable
     VariableDef variable = entity.getVariable(inputVar)
         .orElseThrow(() -> new BadRequestException("Invalid variable spec for output entity"));
-
-    // check that variable is native to entity
-    if (!variable.getSource().isNativeOrId()) {
-      throw new BadRequestException("This plugin only accepts variables native to the output entity.");
-    }
 
     // check that suffix is present
     if (config.getValueSuffix() == null || config.getValueSuffix().contains("\t")) {
