f185b046e6e67f17656123f5e38fcd1baf20405e
Ryan Doherty <tech@conical.org>
2023-08-30 23:27:33 -0400

    Upgrade edacommon

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ repositories {
 
 // project versions
 val containerCore = "6.16.0"
-val edaCommon =     "11.1.0"
+val edaCommon =     "11.2.0"
 
 // use local EdaCommon compiled schema if project exists, else use released version;
 //    this mirrors the way we use local EdaCommon code if available
