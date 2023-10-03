560e67c1e501964f1d0f87b08aa35166f9b1b8a5
Danielle Callan <dcallan@upenn.edu>
2023-10-03 12:10:03 -0400

    use edacommon beta3

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ repositories {
 
 // project versions
 val containerCore = "6.16.0"
-val edaCommon =     "11.5.0-beta2"
+val edaCommon =     "11.5.0-beta3"
 
 // use local EdaCommon compiled schema if project exists, else use released version;
 //    this mirrors the way we use local EdaCommon code if available
