8b3b55cecef817fbb87112766f40c5ee11bdfcad
Danielle Callan <dcallan@upenn.edu>
2023-11-22 11:04:46 -0500

    get the data in R

--- src/main/java/org/veupathdb/service/eda/compute/plugins/correlationassayassay/CorrelationAssayAssayPlugin.java
+++ src/main/java/org/veupathdb/service/eda/compute/plugins/correlationassayassay/CorrelationAssayAssayPlugin.java
@@ -31,8 +31,8 @@ import static org.veupathdb.service.eda.common.plugin.util.PluginUtil.singleQuot
 public class CorrelationAssayAssayPlugin extends AbstractPlugin<CorrelationAssayMetadataPluginRequest, Correlation1Collection> {
   private static final Logger LOG = LogManager.getLogger(CorrelationAssayAssayPlugin.class);
 
-  private static final String ASSAY_1_DATA = "assay1_input";
-  private static final String ASSAY_2_DATA = "assay2_input";
+  private static final String ASSAY_1_DATA = "assay1Data";
+  private static final String ASSAY_2_DATA = "assay2Data";
 
   public CorrelationAssayAssayPlugin(@NotNull PluginContext<CorrelationAssayMetadataPluginRequest, Correlation1Collection> context) {
     super(context);
@@ -78,32 +78,58 @@ public class CorrelationAssayAssayPlugin extends AbstractPlugin<CorrelationAssay
     String method = computeConfig.getCorrelationMethod().getValue();
     CollectionSpec assay1 = computeConfig.getCollectionVariable1();
     CollectionSpec assay2 = computeConfig.getCollectionVariable2();
-    // entity ids are validated as the same by now
-    String entityId = assay1.getEntityId();
-
+    // identify if were on the same entity, or need to work in the parent entity space
+    String entity1Id = assay1.getEntityId();
+    String entity2Id = assay2.getEntityId();
+    boolean isSameEntity = entity1Id.equals(entity2Id);
+    
     // Wrangle into helpful types
-    EntityDef entity = metadata.getEntity(entityId).orElseThrow();
-    VariableDef computeEntityIdVarSpec = util.getEntityIdVarSpec(entityId);
-    String computeEntityIdColName = util.toColNameOrEmpty(computeEntityIdVarSpec);
+    EntityDef entity1 = metadata.getEntity(entity1Id).orElseThrow();
+    EntityDef entity2 = metadata.getEntity(entity2Id).orElseThrow();
+    EntityDef parentEntity = metadata.getAncestors(entity1).get(0);
+    VariableDef entity1IdVarSpec = util.getEntityIdVarSpec(entity1Id);
+    VariableDef parentEntityIdVarSpec = util.getEntityIdVarSpec(parentEntity.getId());
+    String computeEntityIdColName = isSameEntity ? util.toColNameOrEmpty(entity1IdVarSpec) : util.toColNameOrEmpty(parentEntityIdVarSpec);
 
     // Get record id columns
-    List<VariableDef> idColumns = new ArrayList<>();
-    for (EntityDef ancestor : metadata.getAncestors(entity)) {
-      idColumns.add(ancestor.getIdColumnDef());
+    List<VariableDef> entity1AncestorIdColumns = new ArrayList<>();
+    for (EntityDef ancestor : metadata.getAncestors(entity1)) {
+      entity1AncestorIdColumns.add(ancestor.getIdColumnDef());
     }
+    List<VariableDef> entity2AncestorIdColumns = new ArrayList<>();
+    for (EntityDef ancestor : metadata.getAncestors(entity2)) {
+      entity2AncestorIdColumns.add(ancestor.getIdColumnDef());
+    }
+    // if were not on the same entity, we can remove the assay entity id from the id columns
+    if (!isSameEntity) {
+      entity1AncestorIdColumns.remove(entity1IdVarSpec);
+      entity2AncestorIdColumns.remove(entity2IdVarSpec);
+    }
+    
 
     HashMap<String, InputStream> dataStream = new HashMap<>();
-    dataStream.put(INPUT_DATA, getWorkspace().openStream(INPUT_DATA));
+    dataStream.put(ASSAY_1_DATA, getWorkspace().openStream(ASSAY_1_DATA));
+    dataStream.put(ASSAY_2_DATA, getWorkspace().openStream(ASSAY_2_DATA));
 
     RServe.useRConnectionWithRemoteFiles(dataStream, connection -> {
       connection.voidEval("print('starting correlation computation')");
 
-      // Read in the abundance data
-      List<VariableSpec> abundanceInputVars = ListBuilder.asList(computeEntityIdVarSpec);
-      abundanceInputVars.addAll(util.getCollectionMembers(collectionVariable));
-      abundanceInputVars.addAll(idColumns);
-      connection.voidEval(util.getVoidEvalFreadCommand(INPUT_DATA, abundanceInputVars));
-      connection.voidEval("abundanceData <- " + INPUT_DATA); // Renaming here so we can go get the sampleMetadata later
+      // TODO:
+      // need to build two AbundanceData objects
+      // if were not on the same entity, we need to use the parent id column to validate the data/ rows are from the same samples
+      // should be able to do that by using the parent id column in both as recordIdColumn in both AbundanceData objects
+
+
+      // Read in the assay data
+      List<VariableSpec> assay1InputVars = ListBuilder.asList(computeEntityIdVarSpec);
+      assay1InputVars.addAll(util.getCollectionMembers(assay1));
+      assay1InputVars.addAll(entity1AncestorIdColumns);
+      connection.voidEval(util.getVoidEvalFreadCommand(ASSAY_1_DATA, assay1InputVars));
+
+      List<VariableSpec> assay2InputVars = ListBuilder.asList(computeEntityIdVarSpec);
+      assay2InputVars.addAll(util.getCollectionMembers(assay2));
+      assay2InputVars.addAll(entity2AncestorIdColumns);
+      connection.voidEval(util.getVoidEvalFreadCommand(ASSAY_2_DATA, assay2InputVars));
 
       // Read in the sample metadata
       List <VariableSpec> metadataInputVars = ListBuilder.asList(computeEntityIdVarSpec);
